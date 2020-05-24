4aaecb32e545449d9cdaf7919ed81ada8c88d93e
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-24 14:51:21 -0700

    MakeArrayConsecutive2

---
+++ MakeArrayConsecutive2.py
@@ -0,0 +1,9 @@
+def makeArrayConsecutive2(statues):
+    count = 0
+    sorted_list = sorted(statues)
+    for i in range(len(sorted_list)-1):
+        if sorted_list[i+1] == sorted_list[i]+1:
+            continue
+        else:
+            count += (sorted_list[i+1] - sorted_list[i]) - 1
+    return count
