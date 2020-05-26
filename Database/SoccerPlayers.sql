b383fba398830b262098d58c916dd769942a1d6d
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-26 15:03:48 -0700

    SoccerPlayers

---
+++ Database/SoccerPlayers.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE soccerPlayers()
+BEGIN
+    SELECT GROUP_CONCAT(first_name,' ', surname,' #',player_number ORDER BY player_number ASC SEPARATOR '; ') as "players"
+    FROM soccer_team;
+	
+END
